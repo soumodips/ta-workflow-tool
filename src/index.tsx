e810f0cf71ebecebbd6c95b5cb209c044d750f34
soumodips <smdpsinha@gmail.com>
2025-02-23 20:24:50 +0530

    folder restructure and import fixes3

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './common/styles/index.css';
-import reportWebVitals from './helpers/reportWebVitals';
+import reportWebVitals from './maintenance/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
